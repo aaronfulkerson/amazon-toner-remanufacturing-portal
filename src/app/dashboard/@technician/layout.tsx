c905ada3db990953dfd06d52d8b287781a433493
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-04-19 00:12:34 -0600

    add customer and technician dashboards

---
+++ src/app/dashboard/@technician/layout.tsx
@@ -0,0 +1,7 @@
+export default async function TechnicianDashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return children;
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,17 +4,23 @@ import { ROUTES } from "@/modules";
 
 export default async function DashboardLayout({
   admin,
+  customer,
   employee,
+  technician,
 }: Readonly<{
   admin: React.ReactNode;
+  customer: React.ReactNode;
   employee: React.ReactNode;
+  technician: React.ReactNode;
 }>) {
   const { session, user } = await getCurrentSession();
   if (!session) redirect(ROUTES.LOGIN);
 
   const component = {
     admin,
+    customer,
     employee,
+    technician,
   };
 
   return component[user.role];
