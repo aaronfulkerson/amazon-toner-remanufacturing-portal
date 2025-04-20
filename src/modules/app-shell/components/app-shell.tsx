31c5ba8931e749164658ba900125b85bcebc8f96
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-04-20 11:28:51 -0600

    sidebar placeholder

--- src/modules/app-shell/components/app-shell.tsx
+++ src/modules/app-shell/components/app-shell.tsx
@@ -1,7 +1,26 @@
+import { Sidebar } from "@/modules/app-shell";
+
+interface AppShellContentProps {
+  children: React.ReactNode;
+}
+
+function AppShellContent({ children }: AppShellContentProps) {
+  return (
+    <main className="min-h-screen py-10 pl-72">
+      <div className="px-8 overflow-y-auto">{children}</div>
+    </main>
+  );
+}
+
 interface AppShellProps {
   children: React.ReactNode;
 }
 
 export function AppShell({ children }: AppShellProps) {
-  return <main>{children}</main>;
+  return (
+    <div className="min-h-screen">
+      <Sidebar />
+      <AppShellContent>{children}</AppShellContent>
+    </div>
+  );
 }
