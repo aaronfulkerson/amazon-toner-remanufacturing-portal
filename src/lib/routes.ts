a01776293f23d94e642c974a020ffe51e38fab62
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-05-04 10:15:01 -0600

    add default conversionFn

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,9 +1,9 @@
 import type { NextRequest } from "next/server";
 
-export function getParam<T>(
+export function getParam<T = string>(
   request: NextRequest,
   paramName: string,
-  conversionFn: (param: string) => T
+  conversionFn: (param: string) => T = (param) => String(param) as T
 ): T | undefined {
   const param = request.nextUrl.searchParams.get(paramName);
   return param ? conversionFn(param) : undefined;
