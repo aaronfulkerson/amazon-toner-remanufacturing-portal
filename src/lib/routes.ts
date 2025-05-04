05bfb9669248056776e2bcfb833e0cc70ee1b28f
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-05-04 10:34:05 -0600

    overload getParam

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,10 +1,19 @@
 import type { NextRequest } from "next/server";
 
-export function getParam<T = string>(
+export function getParam(
+  request: NextRequest,
+  paramName: string
+): string | undefined;
+export function getParam<T>(
   request: NextRequest,
   paramName: string,
-  conversionFn: (param: string) => T = (param) => String(param) as T
-): T | undefined {
+  conversionFn: (param: string) => T
+): T | undefined;
+export function getParam(
+  request: NextRequest,
+  paramName: string,
+  conversionFn: (param: string) => unknown = (param) => String(param)
+): unknown {
   const param = request.nextUrl.searchParams.get(paramName);
   return param ? conversionFn(param) : undefined;
 }
