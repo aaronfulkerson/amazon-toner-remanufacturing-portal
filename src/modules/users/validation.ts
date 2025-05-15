126b21c2ec7c707488d90a489a9ee185d1faddee
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-05-14 22:15:50 -0600

    correctly validate form data

--- src/modules/users/validation.ts
+++ src/modules/users/validation.ts
@@ -4,8 +4,11 @@ import { createUserSchema } from "@/modules/users";
 import type { CreateUserSchema } from "@/modules/users";
 
 export async function validate(formData: FormData): Promise<CreateUserSchema> {
-  const unparsed = Object.fromEntries(formData);
-  const { data, success } = createUserSchema.safeParse(unparsed);
+  const unparsed = Object.fromEntries(formData.entries());
+  const { data, success } = createUserSchema.safeParse({
+    ...unparsed,
+    permissions: formData.getAll("permissions"),
+  });
   if (!success) throw Error(FORM_ERRORS.VALIDATION_FAILED);
 
   return data;
