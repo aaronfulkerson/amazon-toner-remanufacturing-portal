903afab1c7225ab6d630932fa46ddc215ba7305c
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-05-17 23:54:45 -0600

    reorder imports

--- src/db/queries/types.ts
+++ src/db/queries/types.ts
@@ -1,6 +1,7 @@
-import { TablesRelationalConfig } from "drizzle-orm";
-import { NeonTransaction } from "drizzle-orm/neon-serverless";
 import { db } from "@/db";
+
+import type { TablesRelationalConfig } from "drizzle-orm";
+import type { NeonTransaction } from "drizzle-orm/neon-serverless";
 
 export type DbExecutor =
   | NeonTransaction<typeof db._.fullSchema, TablesRelationalConfig>
