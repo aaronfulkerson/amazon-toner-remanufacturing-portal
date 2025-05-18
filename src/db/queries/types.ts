5cf4b7d53e08c0ceafcfa4694ba433e30a21c6ef
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-05-17 23:52:22 -0600

    rename TransactionOrDb to DbExecutor

--- src/db/queries/types.ts
+++ src/db/queries/types.ts
@@ -2,6 +2,6 @@ import { TablesRelationalConfig } from "drizzle-orm";
 import { NeonTransaction } from "drizzle-orm/neon-serverless";
 import { db } from "@/db";
 
-export type TransactionOrDb =
+export type DbExecutor =
   | NeonTransaction<typeof db._.fullSchema, TablesRelationalConfig>
   | typeof db;
