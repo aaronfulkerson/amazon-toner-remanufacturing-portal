fb7e65419d32de8a847a61a6091f64b0e29a7a9b
Aaron Fulkerson <aaron.fulkerson@icloud.com>
2025-06-08 17:22:56 -0600

    obey server redirect in client

--- src/hooks/use-query-with-toast.ts
+++ src/hooks/use-query-with-toast.ts
@@ -1,4 +1,7 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useToast } from "@/components/toast.context";
 
@@ -9,19 +12,26 @@ export function useQueryWithToast<TQueryFnData>(
   fetchUrl: string,
   options: UseQueryOptions<TQueryFnData, ServerResult>
 ) {
+  const router = useRouter();
   const { createToast } = useToast();
 
   const { error, ...rest } = useQuery({
     queryFn: async () => {
       const response = await fetch(fetchUrl);
-      if (response.status !== 200) throw await response.json();
+      if (response.redirected) {
+        router.replace(response.url);
+        router.refresh();
+      }
+      if (response.status !== 200) {
+        throw await response.json();
+      }
       return await response.json();
     },
     ...options,
   });
 
   useEffect(() => {
-    if (error) createToast(error);
+    if (error?.type) createToast(error);
   }, [createToast, error]);
 
   return { error, ...rest };
